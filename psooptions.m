de3fedeac746fb0ffa733c9d90b2c955711caaf4
Amin Gholizad <amin.gholizad@gmail.com>
2022-02-21 03:15:07 +0330

    bug fix The algorithm as a whole did not used infeasibility!

--- psooptions.m
+++ psooptions.m
@@ -3,7 +3,7 @@ function options = psooptions()
 options.max_iter = 200; %maximum iterations
 options.lower_bound = [0,0,0,0]; %lower bound of vars
 options.upper_bound = [pi/2,pi/2,pi/2,pi/2]; %upper bound of vars
-options.swarm_size=20; % swarm size
+options.swarm_size=200; % swarm size
 options.problem=@cost_fcn; % objective function and constraint function
 %% pso coefficients
 options.c = [0.1,0.2]; %[cognitive acceleration, social acceleration] coefficients
